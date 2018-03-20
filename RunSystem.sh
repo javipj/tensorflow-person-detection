730c06b6490883921b629dcc196ca7628a78eaee
DataScienceNow <david.salvador@sofwerx.org>
2018-03-20 09:41:13 -0400

    Commit message

--- RunSystem.sh
+++ RunSystem.sh
@@ -1,6 +1,1 @@
-(python PersonDetectionCamera.py) &
-while true; do
-    
-    ffmpeg -fflags discardcorrupt -i rtsp://admin:888888@192.168.0.164:10554/udp/av0_0 -f image2 -updatefirst 1 img.jpg -y
-    
-done 
+./GetImages.sh & python PersonDetectionCamera.py 
